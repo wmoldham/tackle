ba152712d5a76b460b32aa71971b87ef7f9859d7
William Oldham <wm.oldham@gmail.com>
2023-01-24 09:12:13 -0500

    refactor sample and standard slots to be row numbers

--- R/Assay-class.R
+++ R/Assay-class.R
@@ -4,13 +4,14 @@
 setClass(
   "Assay",
   slots = c(
+    raw = "data.frame",
     model = "function",
     fit = "lm",
     quality = "numeric",
     x_col = "character",
     y_col = "character",
-    standards = "data.frame",
-    samples = "data.frame",
+    standards = "numeric",
+    samples = "numeric",
     clean = "data.frame"
   )
 )
@@ -27,15 +28,17 @@ setMethod(
   "initialize",
   "Assay",
   function(.Object, raw, model) {
+    .Object@raw <- raw
     .Object@model <- model
     .Object@fit <- model(raw)
     .Object@quality <- summary(.Object@fit)$r.squared
-    .Object@x_col <- deparse(fit$terms[[3]])
-    .Object@y_col <- deparse(fit$terms[[2]])
-    .Object@standards <- raw[!is.na(raw[[.Object@x_col]]), ]
-    .Object@samples <- raw[is.na(raw[[.Object@x_col]]), ]
+    .Object@x_col <- deparse(.Object@fit$terms[[3]])
+    .Object@y_col <- deparse(.Object@fit$terms[[2]])
+    .Object@standards <- which(!is.na(raw[[.Object@x_col]]))
+    .Object@samples <- which(is.na(raw[[.Object@x_col]]))
     .Object@clean <-
       interpolate(
+        .Object@raw,
         .Object@fit,
         .Object@standards,
         .Object@samples,
@@ -57,9 +60,9 @@ methods::setValidity(
   }
 )
 
-interpolate <- function(fit, standards, samples, x_col, y_col) {
+interpolate <- function(raw, fit, standards, samples, x_col, y_col) {
   p <- polynom::polynomial(stats::coefficients(fit))
-  new_y <- as.list(samples[[y_col]])
+  new_y <- as.list(raw[samples, y_col])
   new_x <- unlist(
     lapply(new_y, \(y) {
       roots <- solve(p, y)
@@ -69,22 +72,22 @@ interpolate <- function(fit, standards, samples, x_col, y_col) {
           which(
             Im(roots) == 0 &
               Re(roots) >= 0 &
-              Re(roots) <= 1.25 * max(standards[[x_col]])
+              Re(roots) <= 1.25 * max(raw[standards, x_col])
           )
         ]
       ifelse(identical(root, numeric(0)), NA, Re(root))
     })
   )
-  samples[[x_col]] <- new_x
-  rbind(standards, samples)
+  raw[samples, x_col] <- new_x
+  raw
 }
 
 setMethod(
   "show",
   "Assay",
   function(object) {
-    standards <- nrow(object@standards)
-    samples <- nrow(object@samples)
+    standards <- length(object@standards)
+    samples <- length(object@samples)
     cat(
       is(object)[[1]], ":\n",
       "- Standards = ", standards, "\n",
@@ -94,5 +97,15 @@ setMethod(
       sep = ""
     )
     print(object@clean)
+  }
+)
+
+setMethod(
+  "print",
+  "Assay",
+  function(x, y, ...) {
+    x@clean |>
+      dplyr::filter()
+    ggplot2::ggplot()
   }
 )
