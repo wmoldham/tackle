0dbeccf2c5ee42eca614b5588b5c6bd50f583444
Will <wm.oldham@gmail.com>
2023-01-23 21:04:40 -0500

    add show method for Assay class

--- R/Assay-class.R
+++ R/Assay-class.R
@@ -4,10 +4,13 @@
 setClass(
   "Assay",
   slots = c(
-    raw = "data.frame",
     model = "function",
     fit = "lm",
     quality = "numeric",
+    x_col = "character",
+    y_col = "character",
+    standards = "data.frame",
+    samples = "data.frame",
     clean = "data.frame"
   )
 )
@@ -24,11 +27,21 @@ setMethod(
   "initialize",
   "Assay",
   function(.Object, raw, model) {
-    .Object@raw <- raw
     .Object@model <- model
     .Object@fit <- model(raw)
     .Object@quality <- summary(.Object@fit)$r.squared
-    .Object@clean <- interpolate(.Object@raw, .Object@fit)
+    .Object@x_col <- deparse(fit$terms[[3]])
+    .Object@y_col <- deparse(fit$terms[[2]])
+    .Object@standards <- raw[!is.na(raw[[.Object@x_col]]), ]
+    .Object@samples <- raw[is.na(raw[[.Object@x_col]]), ]
+    .Object@clean <-
+      interpolate(
+        .Object@fit,
+        .Object@standards,
+        .Object@samples,
+        .Object@x_col,
+        .Object@y_col
+      )
 
     methods::validObject(.Object)
     .Object
@@ -44,20 +57,42 @@ methods::setValidity(
   }
 )
 
-interpolate <- function(raw, fit) {
-  standards <- raw[!is.na(raw$conc), ]
-  samples <- raw[is.na(raw$conc), ]
-  x <- stats::model.frame(fit)[[deparse(fit$terms[[3]])]]
+interpolate <- function(fit, standards, samples, x_col, y_col) {
   p <- polynom::polynomial(stats::coefficients(fit))
-  new_y <- as.list(samples[[deparse(fit$terms[[2]])]])
+  new_y <- as.list(samples[[y_col]])
   new_x <- unlist(
     lapply(new_y, \(y) {
       roots <- solve(p, y)
       roots <- round(roots, digits = 8)
-      root <- roots[which(Im(roots) == 0 & Re(roots) >= 0 & Re(roots) <= 1.25 * max(x))]
+      root <-
+        roots[
+          which(
+            Im(roots) == 0 &
+              Re(roots) >= 0 &
+              Re(roots) <= 1.25 * max(standards[[x_col]])
+          )
+        ]
       ifelse(identical(root, numeric(0)), NA, Re(root))
     })
   )
-  samples$conc <- new_x
+  samples[[x_col]] <- new_x
   rbind(standards, samples)
 }
+
+setMethod(
+  "show",
+  "Assay",
+  function(object) {
+    standards <- nrow(object@standards)
+    samples <- nrow(object@samples)
+    cat(
+      is(object)[[1]], ":\n",
+      "- Standards = ", standards, "\n",
+      "- Samples   = ", samples, "\n",
+      "- Formula   = ", deparse(object@fit$call), "\n",
+      "- R squared = ", object@quality, "\n\n",
+      sep = ""
+    )
+    print(object@clean)
+  }
+)
