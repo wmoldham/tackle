2d69d8271376c80513c1ac40dc94acb276e7a9df
William Oldham <wm.oldham@gmail.com>
2023-01-30 09:15:03 -0500

    add norm_pqn()

--- R/metabolomics.R
+++ R/metabolomics.R
@@ -24,3 +24,38 @@ impute_rf <- function(se) {
     t()
   se
 }
+
+
+#' Normalize using probabilistic quotient normalization
+#'
+#' This function will perform a probabilistic quotient normalization on the assay
+#' values in a SummarizedExperiment object. If the `rowData` contains a column
+#' named `reference`, these values will be used for normalization, otherwise, the
+#' mean feature value across all samples will be used.
+#'
+#' @param se A SummarizedExperiment object.
+#'
+#' @return A SummarizedExperiment object with normalized assay values.
+#' @export
+#'
+#' @examples
+#' se <- norm_pqn(se)
+#'
+norm_pqn <- function(se) {
+  mat <- SummarizedExperiment::assay(se)
+
+  has_reference <-
+    SummarizedExperiment::rowData(se) |>
+    names() |>
+    {\(x) "reference" %in% x}()
+
+  if (has_reference) {
+    quotients <- mat / SummarizedExperiment::rowData(se)$reference
+  } else {
+    reference <- apply(mat, 1, mean)
+    quotients <- mat / reference
+  }
+  quotient_medians <- apply(quotients, 2, stats::median)
+  SummarizedExperiment::assay(se) <- t(t(mat) / quotient_medians)
+  se
+}
